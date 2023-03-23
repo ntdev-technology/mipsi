06b8f0c410d33aeecbbda5a731f10fb9dbb04fdb
tommie1236 <timooosterom@gmail.com>
2023-03-23 14:36:46 +0100

    added java files

---
+++ src/mipsi/Mipsi.java
@@ -0,0 +1,13 @@
+package mipsi;
+
+/**
+ * @author timoo
+ *
+ */
+public class Mipsi {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
