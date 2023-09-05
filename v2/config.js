7b8fd404807d64c1703e002a290d84a2f7a4f9fa
tommie1236 <timooosterom@gmail.com>
2023-09-05 17:06:05 +0200

    saved

--- v2/config.js
+++ v2/config.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     const submitButton = document.getElementById('submitButton');
-    const usernameInput = document.getElementById('UsernameInput');
-    const passwordInput = document.getElementById('PasswordInput');
 
     submitButton.addEventListener('click', function () {
 
@@ -13,25 +11,5 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 500);
 
 
-        const username = usernameInput.value;
-        const password = passwordInput.value;
-
-
-        const userData = { username, password };
-
-        fetch('/submit_data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Response form Backend:', data)
-        })
-        .catch(error => {
-            console.error('Error:', error)
-        })
     });
 });
