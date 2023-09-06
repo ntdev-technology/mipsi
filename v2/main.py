8254517cedc97304c0baab24b489ac6cc1514f2d
tommie1236 <timooosterom@gmail.com>
2023-09-06 13:49:04 +0000

    save code ubuntu crash

--- v2/main.py
+++ v2/main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, send_file, redirect, request
 import sqlite3
 import bcrypt
 
+__version__ = '1.0.0'
+
+
 def getDBConn(db: str) -> sqlite3.Connection:
 	conn = sqlite3.connect(f'db/{db}.db')
 	return conn
@@ -11,6 +14,18 @@ def createPwHash(password: str) -> str:
 	hashpw = bcrypt.hashpw(password.encode('utf-8'), salt)
 	shashpw = f'{salt.decode("utf-8")}|{hashpw.decode("utf-8")}'
 	return shashpw
+
+def checkPw(username, password) -> bool:
+	conn = getDBConn('users')
+	cur = conn.cursor()
+	shash: str = cur.execute('SELECT passwordhash FROM users WHERE username=?', (username,)).fetchall()[0][0]
+	salt, hash = shash.split('|')
+	pwhash = bcrypt.hashpw(password.encode('utf-8'), salt.encode('utf-8')).decode('utf-8')
+	if  pwhash == hash:
+		return True
+	else:
+		return False
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ba74a4e1a84eddcc42742bb1e0f2a80abe10ad165b010855'
@@ -31,7 +46,8 @@ def login():
 		case 'POST':
 			print(request.form['username'])
 			print(request.form['password'])
-			return render_template('login.html', status='')
+			
+			return render_template('login.html', status=checkPw(request.form['username'], 'login succesfill' if str(request.form['password']) else 'login failed'))
 		
 
 
@@ -42,7 +58,7 @@ def favicon():
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-	...
+	return render_template('dashboard.html', version=__version__)
 
 
 @app.route('/createaccount', methods=['GET', 'POST'])
@@ -51,8 +67,12 @@ def hash():
 		case 'GET':
 			return render_template('/createaccount.html', text='Your hash will appear here')
 		case 'POST':
-			return render_template('/createaccount.html', text=createPwHash(request.form['password'])
-)
+			return render_template('/createaccount.html', text=createPwHash(request.form['password']))
+		
+
+
+
+		
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000, host='0.0.0.0')
