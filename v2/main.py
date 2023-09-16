485b729564e82588a76e298924bd224e553708c8
tommie1236 <timooosterom@gmail.com>
2023-09-16 17:41:50 +0200

    updated html files

--- v2/main.py
+++ v2/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash, render_template, send_file, redirect, request
-from flask_login import LoginManager, login_manager, UserMixin, login_user, logout_user
+from flask_login import LoginManager, login_manager, UserMixin, login_user, logout_user, current_user
 from flask_security import Security, SQLAlchemySessionUserDatastore, RoleMixin, roles_accepted
 from flask_sqlalchemy import SQLAlchemy
 from termcolor import colored
@@ -13,9 +13,11 @@ import os
 import re
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.0-dev'
 
-	
+__email_pattern__ = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+
+
 def createPwHash(password: str) -> str:
 	salt = bcrypt.gensalt()
 	hashpw = bcrypt.hashpw(password.encode('utf-8'), salt)
@@ -43,44 +45,43 @@ loginmng = LoginManager()
 loginmng.init_app(app)
 
 
-
 #--------------------------------
 # endpoints / html build scripts
 
 @app.route('/')
-def home() -> Callable:
-	return redirect('/login') # Returns main site to the login window.
-
-
-
+def root():
+	return redirect('/login')
+	
 @app.route('/login', methods=['GET', 'POST'])
 def login() -> Callable:
 	match request.method:
 		case 'GET':
 			return render_template('login.html')
 		case 'POST':
+			if current_user.is_authenticated:
+				return redirect('/dashboard')
 			try:
 				usr: User = User.query.filter_by(
-					username=request.form['username']).first()
+				username=request.form['username']).first()
 				if checkPw(usr.passhash, request.form['password']):
 					login_user(usr)
 					print(f"{request.remote_addr} - - {datetime.now().strftime('[%d/%b/%Y %H:%M:%S]')} " + colored(f'"USER "{usr.username}" LOGGED IN', 'magenta'))
 					return redirect('/dashboard')
 				else:
-					flash('Password Incorrect')
+					flash('Username or Password incorrect') # password
 					return render_template('/login.html')	
 			except Exception as e:
 				print(e)
-				flash('Username Invalid') # user doesn't exist
+				flash('Username or Password incorrect ') # user doesn't exist, username
 				return render_template('/login.html')
 
 		
 
 @app.route('/logout')
 def logout():
-	logout_user()
-	flash('logged out')
-	return redirect('/login')
+	if logout_user():
+		flash('logged out')
+		return redirect('/login')
 
 
 @app.route('/favicon.ico')
@@ -89,15 +90,51 @@ def favicon():
 
 
 @app.route('/dashboard')
-# @roles_accepted('Admin')
+@roles_accepted('admin', 'operator', 'player')
 def dashboard():
-	
-	return render_template('dashboard.html', version=__version__, username='testusr')
+	return render_template('dashboard.html', version=__version__, username=current_user.username)
 
-@app.route('/access', methods=['GET', 'POST'])
+@app.route('/access')
+@roles_accepted('admin', 'operator', 'player')
 def access():
-	
-	return render_template('access.html')
+	#flash('Account already registered')
+	return render_template('access.html', version=__version__)
+
+@app.route('/admin') # temp
+def admin():
+	return str(User.query.all())
+
+@app.route('/link_mc_acc', methods=['GET', 'POST'])
+def link_mc_acc():
+	match request.method:
+		case 'GET':
+			if current_user.mcname != None:
+				flash('minecraft account already linked')
+				return redirect('/access')
+			return render_template('/link_mc_acc.html')
+		case 'POST':
+			
+
+
+			flash('Minecraft Account Data Succesfull Linked')
+			return redirect('/access')
+	return render_template('link_mc_acc.html')
+
+@app.route('/add_role')
+def add():
+	role = Role.query.filter_by(id=0).first() # 0: player, 1: operator, 2: admin
+	current_user.roles.append(role)
+	db.session.commit()
+	return "succes"
+
+
+@app.route('/delete')
+def delete():
+	# User.query.filter_by(username='test2').first().delete()
+	current_user.delete()
+	db.session.commit()
+	flash('account deleted')
+	return redirect('/login')
 
 
 @app.route('/createaccount', methods=['GET', 'POST'])
@@ -107,14 +144,16 @@ def hash():
 			return render_template('/createaccount.html')
 		case 'POST':
 			# return "Don't actually create a account right now, it works but i can't delete them yet" # comment out line to activate account registration
-			try:
-				User.query.filter_by(username=request.form['username']).first()
+			if User.query.filter_by(username=request.form['username']).first():
 				flash('Account already exists')
 				return render_template('/createaccount.html')
-			except:
+			else:
 				usr = User(username=request.form['username'],
 						passhash=createPwHash(request.form['password']),
 						email=request.form['email'])
+				
+				role = Role.query.filter_by(id=0).one()
+				usr.roles.append(role)
 
 				db.session.add(usr)
 				db.session.commit()
@@ -147,7 +186,17 @@ class User(db.Model, UserMixin):
 	uuid = db.Column(db.String, unique = True)
 	mcname = db.Column(db.String, unique = True)
 	authenticated = db.Column(db.Boolean, default = False)
-	roles = db.relationship('Role', secondary=roles_users, backref='roled')
+	roles = db.relationship('Role', secondary=roles_users, backref='User')
+
+	def delete(self) -> bool:
+		try:
+			db.session.delete(self)
+			return True
+		except:
+			return False
+
+	def __repr__(self) -> str:
+		return f'&lt {self.username}, roles:{self.roles} &gt' # "&lt" and "&gt" are needed to get a < and > to display in html
 
 	def is_active(self) -> str:
 		return self.active
@@ -178,13 +227,16 @@ class Role(db.Model, RoleMixin):
 	id   = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(10), unique=True)
 
+	def __repr__(self):
+		return f'&lt {self.id}, {self.name} &gt'
+
 
 #-----------
 # app logic		
 
 
-# usrds = SQLAlchemySessionUserDatastore(db.session, User, Role)
-# sec = Security(app, usrds)
+usrds = SQLAlchemySessionUserDatastore(db.session, User, Role)
+sec = Security(app, usrds)
 
 
 
